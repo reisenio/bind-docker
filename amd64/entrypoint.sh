cabb6f57b31e9cde79672175081b0c8e0427ad07
reisenio <45011273+reisenio@users.noreply.github.com>
2022-12-01 13:48:18 +0100

    Update entrypoint.sh

--- amd64/entrypoint.sh
+++ amd64/entrypoint.sh
@@ -6,30 +6,26 @@ set -e
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
 file_env() {
-	local var="$1"
-	local fileVar="${var}_FILE"
-	local def="${2:-}"
-	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-		exit 1
-	fi
-	local val="$def"
-	if [ "${!var:-}" ]; then
-		val="${!var}"
-	elif [ "${!fileVar:-}" ]; then
-		val="$(< "${!fileVar}")"
-	fi
-	export "$var"="$val"
-	unset "$fileVar"
+        local var="$1"
+        local fileVar="${var}_FILE"
+        local def="${2:-}"
+        if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+                echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+                exit 1
+        fi
+        local val="$def"
+        if [ "${!var:-}" ]; then
+                val="${!var}"
+        elif [ "${!fileVar:-}" ]; then
+                val="$(< "${!fileVar}")"
+        fi
+        export "$var"="$val"
+        unset "$fileVar"
 }
 
 file_env 'ROOT_PASSWORD'
 
-ROOT_PASSWORD=${ROOT_PASSWORD:-password}
-WEBMIN_ENABLED=${WEBMIN_ENABLED:-true}
-
 BIND_DATA_DIR=${DATA_DIR}/bind
-WEBMIN_DATA_DIR=${DATA_DIR}/webmin
 
 create_bind_data_dir() {
   mkdir -p ${BIND_DATA_DIR}
@@ -49,23 +45,6 @@ create_bind_data_dir() {
   fi
   rm -rf /var/lib/bind
   ln -sf ${BIND_DATA_DIR}/lib /var/lib/bind
-}
-
-create_webmin_data_dir() {
-  mkdir -p ${WEBMIN_DATA_DIR}
-  chmod -R 0755 ${WEBMIN_DATA_DIR}
-  chown -R root:root ${WEBMIN_DATA_DIR}
-
-  # populate the default webmin configuration if it does not exist
-  if [ ! -d ${WEBMIN_DATA_DIR}/etc ]; then
-    mv /etc/webmin ${WEBMIN_DATA_DIR}/etc
-  fi
-  rm -rf /etc/webmin
-  ln -sf ${WEBMIN_DATA_DIR}/etc /etc/webmin
-}
-
-set_root_passwd() {
-  echo "root:$ROOT_PASSWORD" | chpasswd
 }
 
 create_pid_dir() {
@@ -93,13 +72,6 @@ fi
 
 # default behaviour is to launch named
 if [[ -z ${1} ]]; then
-  if [ "${WEBMIN_ENABLED}" == "true" ]; then
-    create_webmin_data_dir
-    set_root_passwd
-    echo "Starting webmin..."
-    /etc/init.d/webmin start
-  fi
-
   echo "Starting named..."
   exec $(which named) -u ${BIND_USER} -g ${EXTRA_ARGS}
 else
